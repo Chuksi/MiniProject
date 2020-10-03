5bcdfd1058f73fbcce9b0f47ba7e60649c01ed2e
Tony Friedli <tony.friedli@ost.ch>
2020-10-03 09:12:00 +0200

    Test: Drawing onConvas

---
+++ app/src/main/java/com/example/miniproject/Circle.java
@@ -0,0 +1,11 @@
+package com.example.miniproject;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class Circle extends SurfaceView {
+    public Circle(Context context) {
+        super(context);
+
+    }
+}
